1ea3b0ad52c3a0b557704149477dae987c9b2b4c
wuevan510 <wuevan510@gmail.com>
2018-05-09 02:20:15 +0800

    2018-05-09上課內容

---
+++ Lecture04/Ex01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex01
+{
+    internal class Program
+    {
+        private int[] numbers = new int[9];
+        private Random random = new Random();
+        private int total = 0;
+
+            for (int index = 0; index<numbers.Length; index++)
+            {
+                numbers[index] = random.Next(1, 11);
+                total += numbers[index];
+                Console.WriteLine(numbers[index]);
+            }
+
+    Console.WriteLine(total);
+    }
+}
---
+++ Lecture04/Example03/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example03
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //int[] numbers = new int[10];
+            //Random random = new Random();
+
+            ////TODO 產生亂數，不能有重覆的數字
+            ////TODO 產生亂數、挑4個不同的數字
+            //for (int index = 0; index < numbers.Length; index++)
+            //{
+            //    numbers[index] = random.Next(1, 11);
+            //    Console.WriteLine(numbers[index]);
+            //}
+
+            int[] numbers = new int[9];
+            Random random = new Random();
+            int total = 0;
+
+            for (int index = 0; index < numbers.Length; index++)
+            {
+                numbers[index] = random.Next(1, 11);
+                total += numbers[index];
+                Console.WriteLine(numbers[index]);
+            }
+            Console.WriteLine(total);
+
+            //int total = 0;
+
+            //for (int next = 1; next < 1001; next += 2)
+            //{
+            //    total = total + next;
+            //    Console.WriteLine(total);
+            //}
+        }
+    }
+}
